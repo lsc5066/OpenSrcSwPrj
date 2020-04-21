97e4c2669337ea000b2756bb0b280fee452f78b2
lsc5066 <leesc332@gmail.com>
2020-04-21 23:54:18 +0900

    구구단

--- 오픈소스SW입문/src/gugudan/gugudan.java
+++ 오픈소스SW입문/src/gugudan/gugudan.java
@@ -12,26 +12,16 @@ public static void main(String[] args) {
 		System.out.printf("출력할 단을 입력하시오 : ");
 		gu=s.nextInt();
 		
-		if (gu==0) {
-			for(int i=2;i<=9;i++) {
-				for(int j=1;j<=9;j++) {
-					gugudan=i*j;
-					System.out.println(i+"x"+j+"="+gugudan);
+		for(int i=2;i<=9;i++) {
+			gugudan=i*gu;
+			System.out.println(i+"x"+gu+"="+gugudan);
+				
+					
 					
 				}
 				
 			
-		}
 		
-		}
-		else if(gu>0) {
-			for(int i=1;i<=9;i++) {
-				gugudan=gu*i;
-				System.out.println(gu+"x"+i+"="+gugudan);
-			}
-		}else if(gu<0) {
-			System.out.println("구구단 출력이 불가능합니다. 음수입니다.");
-		}
 		
 		
 
