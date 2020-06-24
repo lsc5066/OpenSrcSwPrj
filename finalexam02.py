28f4e5892623a2caa7104c22d9c5904207db7022
lsc5066 <leesc332@gmail.com>
2020-06-24 10:28:40 +0900

    '기말고사2번수정'

--- finalexam02.py
+++ finalexam02.py
@@ -1,6 +1,8 @@
 import math
-print('201911267 컴퓨터공학부 이성찬')
-class Point:
+print('201911267 컴퓨터공학부 이성찬\n')
+
+
+class location:
 
     x=0
     y=0
@@ -19,7 +21,7 @@ class Point:
         print('좌표 값의 합 : \nx : {} y: {}'.format(self.x+other.x,self.y+other.y))
         
 
-p1=Point(1,1)
-p2=Point(2,2)
+p1=location(1,1)
+p2=location(2,2)
 p1.distance(p2)
 p1+p2
