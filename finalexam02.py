83e09a6096c8897d81bb6408aaa9e6837bb0e24f
lsc5066 <leesc332@gmail.com>
2020-06-24 10:22:47 +0900

    '기말고사2번'

---
+++ finalexam02.py
@@ -0,0 +1,25 @@
+import math
+print('201911267 컴퓨터공학부 이성찬')
+class Point:
+
+    x=0
+    y=0
+    
+    def __init__(self,a,b):
+        self.x=a
+        self.y=b
+    
+    def distance(self,other):
+        a=self.x-other.x
+        b=self.y-other.y
+        d=math.sqrt((a*a)+(b*b))
+        print('점과 점 사이의 거리 : \n',format(d,'.4f'))
+    
+    def __add__(self,other):
+        print('좌표 값의 합 : \nx : {} y: {}'.format(self.x+other.x,self.y+other.y))
+        
+
+p1=Point(1,1)
+p2=Point(2,2)
+p1.distance(p2)
+p1+p2
